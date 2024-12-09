66df00483caee285e9a0ddfacd7e0c52c83e124b
modmynitro <modmynitro@gmail.com>
2024-12-09 08:49:55 +0100

    initial commit

---
+++ TestProject/Class.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace TestProject;
+
+public class Class
+{
+    public void Method(MessageBoxButton button = MessageBoxButton.OK)
+    {
+    }
+}
---
+++ TestProjectTests/ClassTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using TestProject;
+
+namespace TestProjectTests;
+
+public sealed class ClassTests
+{
+    [Test]
+    public void TestMethod()
+    {
+       new Class().Method();
+    }
+}
